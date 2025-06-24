2c220b838210b8d0c20ae9f9e3ef5d41754e49b8
nils_kiefer <21311514+nilskiefer@users.noreply.github.com>
2025-06-24 17:31:25 +0200

    clicker4_stm32 board conf

---
+++ boards/arm/clicker4_stm32/board.cmake
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=STM32F767BI" "--speed=4000")
+
+board_runner_args(openocd
+  --cmd-pre-init "source [find interface/stlink.cfg]"
+  --cmd-pre-init "adapter speed 1800"
+  --cmd-pre-init "source [find target/stm32f7x.cfg]"
+  --cmd-pre-init "adapter srst delay 100"
+)
+
+# board_runner_args(openocd --cmd-pre-init "source [find interface/stlink.cfg]")
+# board_runner_args(openocd --cmd-pre-init "source [find target/stm32f7x.cfg]")
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+
