08bfe3d041580cd00062c5339882ee99573e555f
nils_kiefer <21311514+nilskiefer@users.noreply.github.com>
2025-07-21 16:45:52 +0200

    compiles blinky sample

--- boards/arm/clicker4_stm32f7/board.cmake
+++ boards/arm/clicker4_stm32f7/board.cmake
@@ -1,16 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=STM32F745VGT6" "--speed=4000")
-
-board_runner_args(openocd
-  --cmd-pre-init "source [find interface/stlink.cfg]"
-  --cmd-pre-init "adapter speed 1800"
-  --cmd-pre-init "source [find target/stm32f7x.cfg]"
-  --cmd-pre-init "adapter srst delay 100"
-)
-
-# board_runner_args(openocd --cmd-pre-init "source [find interface/stlink.cfg]")
-# board_runner_args(openocd --cmd-pre-init "source [find target/stm32f7x.cfg]")
+board_runner_args(jlink "--device=STM32F745VG" "--speed=4000")
+board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
