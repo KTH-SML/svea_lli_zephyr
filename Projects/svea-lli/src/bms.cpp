2f8da7a0e4716ad2dd7acb073f24d3ff99b6a2cb
nils_kiefer <21311514+nilskiefer@users.noreply.github.com>
2025-10-14 20:30:57 +0200

    no debug prints

--- Projects/svea-lli/src/bms.cpp
+++ Projects/svea-lli/src/bms.cpp
@@ -194,6 +194,7 @@ static void soc_indicator_init(void) {
 }
 
 static void print_bms_status(const Bms *bms) {
+    return; // disable detailed BMS status prints
     if (bms->status.pack_current > peak_charge_a) {
         peak_charge_a = bms->status.pack_current;
     }
